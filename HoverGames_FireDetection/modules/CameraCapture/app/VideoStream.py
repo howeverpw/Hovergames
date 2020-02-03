71f4e589bc68afc95d4d7a15b4cd187636cb87d0
Prabu <prabu4online@outlook.com>
2020-02-02 17:52:40 -0700

    Add files via upload

---
+++ HoverGames_FireDetection/modules/CameraCapture/app/VideoStream.py
@@ -0,0 +1,69 @@
+
+from __future__ import absolute_import
+
+from threading import Thread
+import sys
+if sys.version_info[0] < 3:  # e.g python version <3
+    import cv2
+else:
+    import cv2
+    from cv2 import cv2
+
+
+
+
+if sys.version_info >= (3, 0):
+    from queue import Queue
+
+else:
+    from Queue import Queue
+
+
+
+class VideoStream(object):
+    def __init__(self, path, queueSize=3):
+        self.stream = cv2.VideoCapture(path)
+        self.stopped = False
+        self.Q = Queue(maxsize=queueSize)
+
+    def start(self):
+        # start a thread to read frames from the video stream
+        t = Thread(target=self.update, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def update(self):
+        try:
+            while True:
+                if self.stopped:
+                    return
+
+                if not self.Q.full():
+                    (grabbed, frame) = self.stream.read()
+
+                    # if the `grabbed` boolean is `False`, then we have
+                    # reached the end of the video file
+                    if not grabbed:
+                        self.stop()
+                        return
+
+                    self.Q.put(frame)
+
+                    # Clean the queue to keep only the latest frame
+                    while self.Q.qsize() > 1:
+                        self.Q.get()
+        except Exception as e:
+            print("got error: "+str(e))
+
+    def read(self):
+        return self.Q.get()
+
+    def more(self):
+        return self.Q.qsize() > 0
+
+    def stop(self):
+        self.stopped = True
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.stream.release()
---
+++ HoverGames_FireDetection/modules/CameraCapture/app/main.py
@@ -0,0 +1,111 @@
+
+
+import os
+import random
+import sys
+import time
+
+
+import iothub_client
+
+from iothub_client import (IoTHubModuleClient, IoTHubClientError, IoTHubError,
+                           IoTHubMessage, IoTHubMessageDispositionResult,
+                           IoTHubTransportProvider)
+
+import CameraCapture
+from CameraCapture import CameraCapture
+
+
+# global counters
+SEND_CALLBACKS = 0
+
+
+def send_to_Hub_callback(strMessage):
+    message = IoTHubMessage(bytearray(strMessage, 'utf8'))
+    hubManager.send_event_to_output("output1", message, 0)
+
+
+
+
+def send_confirmation_callback(message, result, user_context):
+    global SEND_CALLBACKS
+    SEND_CALLBACKS += 1
+
+
+class HubManager(object):
+
+    def __init__(
+            self,
+            messageTimeout,
+            protocol,
+            verbose):
+
+        self.messageTimeout = messageTimeout
+        self.client_protocol = protocol
+        self.client = IoTHubModuleClient()
+        self.client.create_from_environment(protocol)
+        self.client.set_option("messageTimeout", self.messageTimeout)
+        self.client.set_option("product_info", "edge-camera-capture")
+        if verbose:
+            self.client.set_option("logtrace", 1)  # enables MQTT logging
+
+    def send_event_to_output(self, outputQueueName, event, send_context):
+        self.client.send_event_async(
+            outputQueueName, event, send_confirmation_callback, send_context)
+
+
+def main(
+        videoPath,
+        imageProcessingEndpoint="",
+        imageProcessingParams="",
+        showVideo=False,
+        verbose=False,
+        loopVideo=True,
+        convertToGray=False,
+        resizeWidth=0,
+        resizeHeight=0,
+        annotate=False
+):
+     try:
+
+        try:
+            global hubManager
+            hubManager = HubManager(
+                10000, IoTHubTransportProvider.MQTT, verbose)
+        except IoTHubError as iothub_error:
+            return
+        with CameraCapture(videoPath, imageProcessingEndpoint, imageProcessingParams, showVideo, verbose, loopVideo, convertToGray, resizeWidth, resizeHeight, annotate, send_to_Hub_callback) as cameraCapture:
+            cameraCapture.start()
+    except KeyboardInterrupt:
+        print("Camera capture module stopped")
+
+
+def __convertStringToBool(env):
+    if env in ['True', 'TRUE', '1', 'y', 'YES', 'Y', 'Yes']:
+        return True
+    elif env in ['False', 'FALSE', '0', 'n', 'NO', 'N', 'No']:
+        return False
+    else:
+        raise ValueError('Could not convert string to bool.')
+
+
+if __name__ == '__main__':
+    try:
+        VIDEO_PATH = os.environ['VIDEO_PATH']
+        IMAGE_PROCESSING_ENDPOINT = os.getenv('IMAGE_PROCESSING_ENDPOINT', "")
+        IMAGE_PROCESSING_PARAMS = os.getenv('IMAGE_PROCESSING_PARAMS', "")
+        SHOW_VIDEO = __convertStringToBool(os.getenv('SHOW_VIDEO', 'False'))
+        VERBOSE = __convertStringToBool(os.getenv('VERBOSE', 'False'))
+        LOOP_VIDEO = __convertStringToBool(os.getenv('LOOP_VIDEO', 'True'))
+        CONVERT_TO_GRAY = __convertStringToBool(
+            os.getenv('CONVERT_TO_GRAY', 'False'))
+        RESIZE_WIDTH = int(os.getenv('RESIZE_WIDTH', 0))
+        RESIZE_HEIGHT = int(os.getenv('RESIZE_HEIGHT', 0))
+        ANNOTATE = __convertStringToBool(os.getenv('ANNOTATE', 'False'))
+
+    except ValueError as error:
+        print(error)
+        sys.exit(1)
+
+    main(VIDEO_PATH, IMAGE_PROCESSING_ENDPOINT, IMAGE_PROCESSING_PARAMS, SHOW_VIDEO,
+         VERBOSE, LOOP_VIDEO, CONVERT_TO_GRAY, RESIZE_WIDTH, RESIZE_HEIGHT, ANNOTATE)
